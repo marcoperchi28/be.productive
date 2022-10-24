848adb381d052afe233eb7b72749caa97dc6d0f0
marcoperchi28 <marcoemiky78@gmail.com>
2022-10-24 09:30:53 +0200

    Kick starter

---
+++ be_productive/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'be_productive',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
